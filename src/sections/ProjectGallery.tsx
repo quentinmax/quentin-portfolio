6125f2958d6632d4c0ec64edd86e101bf1958553
Quentin Höhne <quentinhohne@s-8d3a238c.on.site.uni-stuttgart.de>
2024-11-08 12:37:48 +0100

    added personal images for gallery view

--- src/sections/ProjectGallery.tsx
+++ src/sections/ProjectGallery.tsx
@@ -7,7 +7,9 @@ import {
   useMotionValueEvent,
 } from "framer-motion";
 import React, { useRef } from "react";
-import Screen from "../assets/images/energieberater-screen.png";
+import img1 from "../../public/gallery/Details.png";
+import img2 from "../../public/gallery/Energieberater.png";
+import img3 from "../../public/gallery/Landing.jpeg";
 
 const ProjectGallery = () => {
   const container = useRef(null);
@@ -33,13 +35,15 @@ const ProjectGallery = () => {
     [0, window.innerHeight * 3.5]
   );
 
+  //TODO: Change image resolution
+
   return (
     <div>
       <div ref={container} className="gallery-container">
-        <Column images={["1", "2", "3"]} y={y1} />
-        <Column images={["1", "2", "3"]} y={y2} />
-        <Column images={["1", "2", "3"]} y={y3} />
-        <Column images={["1", "2", "3"]} y={y4} />
+        <Column images={[img2, img3, img1]} y={y1} />
+        <Column images={[img1, img3, img2]} y={y2} />
+        <Column images={[img3, img2, img1]} y={y3} />
+        <Column images={[img2, img1, img3]} y={y4} />
       </div>
     </div>
   );
@@ -58,10 +62,10 @@ const Column = ({
         return (
           <div key={index} className="column-image-container">
             <img
-              src={`https://picsum.photos/id/${
-                (index + 1) * Math.floor(Math.random() * 20)
-              }/450/600`}
-              // src={Screen}
+              // src={`https://picsum.photos/id/${
+              //   (index + 1) * Math.floor(Math.random() * 20)
+              // }/450/600`}
+              src={src}
               alt="image"
             />
           </div>
